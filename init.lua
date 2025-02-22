540cda306d04b4972b06000da4339a63f9db7569
Maurice Mohlek <maurice.mohlek@valtech.com>
2025-02-23 00:08:20 +0100

    disable dashboard as it causes errors

--- init.lua
+++ init.lua
@@ -241,7 +241,7 @@ require('lazy').setup({
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  { 'numToStr/Comment.nvim',    opts = {} },
 
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
@@ -285,7 +285,7 @@ require('lazy').setup({
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
 
-  { -- Useful plugin to show you pending keybinds.
+  {                     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
@@ -331,7 +331,7 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
-        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
@@ -371,7 +371,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -471,7 +471,7 @@ require('lazy').setup({
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
-      { 'folke/neoconf.nvim', cmd = 'Neoconf', config = true },
+      { 'folke/neoconf.nvim',      cmd = 'Neoconf', config = true },
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
@@ -1049,23 +1049,23 @@ vim.o.shiftwidth = 4
 -- vim: ts=2 sts=2 sw=2 et
 --
 
-local db = require 'dashboard'
-db.setup {
-  theme = 'hyper',
-  config = {
-    week_header = {
-      enable = true,
-    },
-    shortcut = {
-      { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-      {
-        icon = ' ',
-        icon_hl = '@variable',
-        desc = 'Files',
-        group = 'Label',
-        action = 'Telescope find_files',
-        key = 'f',
-      },
-    },
-  },
-}
+-- local db = require 'dashboard'
+-- db.setup {
+--   theme = 'hyper',
+--   config = {
+--     week_header = {
+--       enable = true,
+--     },
+--     shortcut = {
+--       { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+--       {
+--         icon = ' ',
+--         icon_hl = '@variable',
+--         desc = 'Files',
+--         group = 'Label',
+--         action = 'Telescope find_files',
+--         key = 'f',
+--       },
+--     },
+--   },
+-- }
